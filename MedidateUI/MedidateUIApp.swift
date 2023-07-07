fd23d5b8dcee9ff0cc93589dcbbb46a8d3379b97
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-07 21:29:59 +0200

    Initial Commit

---
+++ MedidateUI/MedidateUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedidateUIApp.swift
+//  MedidateUI
+//
+//  Created by Cristina on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct MedidateUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
