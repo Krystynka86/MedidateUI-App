1e51f2db5a5c14a1de147d8d691aca8454ca9100
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-10 18:37:33 +0200

    and custom gestures and sound effects

--- MedidateUI/Screens/HomeView.swift
+++ MedidateUI/Screens/HomeView.swift
@@ -11,6 +11,7 @@ struct HomeView: View {
     // MARK: - PROPERTY
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
     
     // MARK: - BODY
     
@@ -25,7 +26,12 @@ struct HomeView: View {
                 Image("character-2")
                     .resizable()
                     .scaledToFit()
-                .padding()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(Animation
+                        .easeInOut(duration: 4)
+                        .repeatForever()
+                               , value: isAnimating)
             }
             
             
@@ -44,7 +50,10 @@ struct HomeView: View {
             
             
             Button(action: {
-                isOnboardingViewActive = true
+                withAnimation {
+                    playSound(sound: "success", type: "m4a")
+                    isOnboardingViewActive = true
+                }
             }) {
                     Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                         .imageScale(.large)
@@ -56,8 +65,14 @@ struct HomeView: View {
             .buttonBorderShape(.capsule)
             .controlSize(.large)
         } // VStack
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+            })
+        }
     }
-}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
